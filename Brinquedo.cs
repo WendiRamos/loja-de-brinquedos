7edbe4e06387659fe8fc04b81c4cc547aef65a82
Wendi Ramos <wendiramos12@gmail.com>
2021-04-28 13:07:53 -0300

    Adicionar arquivos de projeto.

---
+++ Brinquedo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja_de_Brinquedos28
+{
+   public  class Brinquedo
+    {
+        public float Id { get; set; }
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public int Quantidade { get; set; }
+        public string Status { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loja_de_Brinquedos28
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Brinquedo brinquedo = new Brinquedo();
+            brinquedo.Id = 1318;
+            brinquedo.Nome = "GlitterBall";
+            brinquedo.Cor = "Azul";
+            brinquedo.Quantidade = 28;
+            brinquedo.Status = "Disponivel";
+            Console.WriteLine("ID:" + brinquedo.Id);
+            Console.WriteLine("Nome:" + brinquedo.Nome);
+            Console.WriteLine("Cor:" + brinquedo.Cor);
+            Console.WriteLine("Quantidade:" + brinquedo.Quantidade);
+            Console.WriteLine("Status:" + brinquedo.Status);
+            Console.ReadLine();
+
+        }
+    }
+}
