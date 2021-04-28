6c66c0482c973f76e7e7baac4eae24920d4c4f21
Wendi Ramos <wendiramos12@gmail.com>
2021-04-28 17:59:10 -0300

    Entrada de dados.

--- Program.cs
+++ Program.cs
@@ -8,16 +8,36 @@ namespace Loja_de_Brinquedos28
         static void Main(string[] args)
         {
             Brinquedo brinquedo = new Brinquedo();
-            brinquedo.Id = 1318;
-            brinquedo.Nome = "GlitterBall";
-            brinquedo.Cor = "Azul";
-            brinquedo.Quantidade = 28;
-            brinquedo.Status = "Disponivel";
+            Console.WriteLine("Insira o ID:");
+            brinquedo.Id = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Insira o nome:");
+            brinquedo.Nome = Console.ReadLine();
+
+            Console.WriteLine("Insira a cor:");
+            brinquedo.Cor = Console.ReadLine();
+
+            Console.WriteLine("Insira a quatidade:");
+            brinquedo.Quantidade = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Status do Usuário:\n 1-Ativo;\n 2-Inativo;");
+            if (Convert.ToString(Console.ReadLine()) == "1")
+            {
+
+                brinquedo.Status = true;
+            }
+            else
+            {
+                brinquedo.Status = false;
+            };
+
+
             Console.WriteLine("ID:" + brinquedo.Id);
             Console.WriteLine("Nome:" + brinquedo.Nome);
             Console.WriteLine("Cor:" + brinquedo.Cor);
             Console.WriteLine("Quantidade:" + brinquedo.Quantidade);
             Console.WriteLine("Status:" + brinquedo.Status);
+
             Console.ReadLine();
 
         }
